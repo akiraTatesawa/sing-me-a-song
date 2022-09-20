b563f2ef0bd9e527203dfc4222a3a2c22fa91479
Arthur Akira <arthurak10.at@gmail.com>
2022-09-20 11:00:22 -0300

    chore: setup configs and remove .js extension

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,14 +1,13 @@
 import cors from "cors";
 import express from "express";
 import "express-async-errors";
-import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware.js";
-import recommendationRouter from "./routers/recommendationRouter.js";
+import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware";
+import recommendationRouter from "./routers/recommendationRouter";
 
-const app = express();
+export const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use("/recommendations", recommendationRouter);
 app.use(errorHandlerMiddleware);
 
-export default app;
